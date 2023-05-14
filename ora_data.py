2a267487c44693e7e7ac00b7f0ec615dd9f6d3a5
constantinus345 <96290754+constantinus345@users.noreply.github.com>
2023-05-14 14:03:06 +0300

    timex

---
+++ ora_data.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+orax = datetime.now()
+orax_formatata = orax.strftime("%A %d %B %Y ora %H")
+print(orax_formatata)
+# link formatare https://www.w3schools.com/python/python_datetime.asp
+"ziua luna anul ora"
