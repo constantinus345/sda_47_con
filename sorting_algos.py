1f60c0f1c1c17302fe3ac1903659e0e851d64633
constantinus345 <96290754+constantinus345@users.noreply.github.com>
2023-05-20 10:19:50 +0300

    insert, bubble, merge sort

--- sorting_algos.py
+++ sorting_algos.py
@@ -34,46 +34,66 @@
 # listx_insertion_sort = insertion_sort(listx)
 # print(listx_insertion_sort)
 
-def merge(arr_left, arr_right):
-    #intializam rezultatul
-    result = []
+# def merge(arr_left, arr_right):
+#     #intializam rezultatul
+#     result = []
+#
+#     #initializam index pentru ambele liste
+#     i = j =0
+#
+#     #Merge la ambele liste pana cand una din ele este goala
+#     while i < len(arr_left) and j < len(arr_right):
+#         if arr_left[i] < arr_right[j]:
+#             result.append(arr_left[i])
+#             i += 1
+#         else:
+#             result.append(arr_right[j])
+#             j += 1
+#
+#     #Adaugam elementele ramase dupa traversarea listelor
+#     result.extend(arr_right[j:])
+#     result.extend(arr_left[i:])
+#
+#     return result
+#
+# def merge_sort(arr):
+#
+#     #in caz in care lista are 0 sau 1 elemente, returnam lista insasi
+#     if len(arr) <= 1:
+#         return arr
+#
+#     #Impartim lista in 2 subliste aprox egale
+#     mid = len(arr)//2 #// inseamna imprartit fara rest
+#     left = arr[:mid]
+#     right = arr[mid:]
+#
+#     #sortam listele create left & right folosing functia merge_sort intr-o forma recurenta
+#     #recurent = se apeleaza pe ea insasi in interiorul functiei
+#     left = merge_sort(left)
+#     right = merge_sort(right)
+#
+#     return merge(left, right)
+#
+# listx = [1, 454, 32352, 443, 6436, 34754, 545435, 54353463, 5453]
+# listx_merge_sort = merge_sort(listx)
+# print(f"lista sortata prin merge sort = {listx_merge_sort}")
 
-    #initializam index pentru ambele liste
-    i = j =0
+def selection_sort(arr):
+    #arr vine de la array, adica o structura de date list
+    n = len(arr)
 
-    #Merge la ambele liste pana cand una din ele este goala
-    while i < len(arr_left) and j < len(arr_right):
-        if arr_left[i] < arr_right[j]:
-            result.append(arr_left[i])
-            i += 1
-        else:
-            result.append(arr_right[j])
-            j += 1
+    #Traversam lista pe toata lungimea sa
+    for i in range(n):
+        i_initial = i
+        #traversam lista ramasa dupa indexul i
+        for j in range(i+1, n):
+            if arr[j] < arr[i_initial]:
+                i_initial = j
 
-    #Adaugam elementele ramase dupa traversarea listelor
-    result.extend(arr_right[j:])
-    result.extend(arr_left[i:])
+        arr[i], arr[i_initial] = arr[i_initial], arr[i] #swap intre elemente
 
-    return result
-
-def merge_sort(arr):
-
-    #in caz in care lista are 0 sau 1 elemente, returnam lista insasi
-    if len(arr) <= 1:
-        return arr
-
-    #Impartim lista in 2 subliste aprox egale
-    mid = len(arr)//2 #// inseamna imprartit fara rest
-    left = arr[:mid]
-    right = arr[mid:]
-
-    #sortam listele create left & right folosing functia merge_sort intr-o forma recurenta
-    #recurent = se apeleaza pe ea insasi in interiorul functiei
-    left = merge_sort(left)
-    right = merge_sort(right)
-
-    return merge(left, right)
+    return arr
 
 listx = [1, 454, 32352, 443, 6436, 34754, 545435, 54353463, 5453]
-listx_merge_sort = merge_sort(listx)
-print(f"lista sortata prin merge sort = {listx_merge_sort}")
+listx_selection_sort = selection_sort(listx)
+print(f"Lista sortata prin selection_sort = {listx_selection_sort}")
