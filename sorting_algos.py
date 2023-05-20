9ef4cc38ea64463af9b779d3eb97e10e2a65b161
constantinus345 <96290754+constantinus345@users.noreply.github.com>
2023-05-20 10:37:36 +0300

    insert, bubble, merge sort

--- sorting_algos.py
+++ sorting_algos.py
@@ -77,23 +77,54 @@
 # listx = [1, 454, 32352, 443, 6436, 34754, 545435, 54353463, 5453]
 # listx_merge_sort = merge_sort(listx)
 # print(f"lista sortata prin merge sort = {listx_merge_sort}")
+#
+# def selection_sort(arr):
+#     #arr vine de la array, adica o structura de date list
+#     n = len(arr)
+#
+#     #Traversam lista pe toata lungimea sa
+#     for i in range(n):
+#         i_initial = i
+#         #traversam lista ramasa dupa indexul i
+#         for j in range(i+1, n):
+#             if arr[j] < arr[i_initial]:
+#                 i_initial = j
+#
+#         arr[i], arr[i_initial] = arr[i_initial], arr[i] #swap intre elemente
+#
+#     return arr
+#
+# listx = [1, 454, 32352, 443, 6436, 34754, 545435, 54353463, 5453]
+# listx_selection_sort = selection_sort(listx)
+# print(f"Lista sortata prin selection_sort = {listx_selection_sort}")
 
-def selection_sort(arr):
-    #arr vine de la array, adica o structura de date list
-    n = len(arr)
+#Quicksort
 
-    #Traversam lista pe toata lungimea sa
-    for i in range(n):
-        i_initial = i
-        #traversam lista ramasa dupa indexul i
-        for j in range(i+1, n):
-            if arr[j] < arr[i_initial]:
-                i_initial = j
+def partitioning(arr, low, high):
+    #selectez ultimul element din lista ca pivot
+    pivot = arr[high]
 
-        arr[i], arr[i_initial] = arr[i_initial], arr[i] #swap intre elemente
+    #cel mai mic index din afara listei
+    i = low -1
 
-    return arr
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i] #swap, schimb cu locul
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i+1
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partitioning(arr, low, high)
+        quick_sort(arr, low, pi-1)
+        quick_sort(arr, pi+1, high)
+        return arr
+
+    return "error: Low > High"
 
 listx = [1, 454, 32352, 443, 6436, 34754, 545435, 54353463, 5453]
-listx_selection_sort = selection_sort(listx)
-print(f"Lista sortata prin selection_sort = {listx_selection_sort}")
+n = len(listx)
+listx_quicksort = quick_sort(listx, 0, n-1)
+print(f"Lista sortata prin quicksort = {listx_quicksort}")
